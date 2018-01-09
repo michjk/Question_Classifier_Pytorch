dda9007fe90892092d3855ffa64865ef182738ed
Michael Jonathan <michj@gmail.com>
2018-01-10 03:11:34 +0800

    fix bug

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ model.word_embeddings.weight.requires_grad = False
 
 loss_function = nn.NLLLoss()
 update_parameter = filter(lambda p: p.requires_grad, model.parameters())
-optimizer = optim.Adam(model.parameters(), lr = 1e-3)
+optimizer = optim.Adam(update_parameter, lr = 1e-3)
 
 
 # In[10]:
