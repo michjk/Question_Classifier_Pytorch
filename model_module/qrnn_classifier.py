b2059c647317261e89cec6cdad6316405fe551e0
Michael Jonathan <michj@gmail.com>
2018-03-08 04:55:16 +0800

    prototype qrnn compete

--- model_module/qrnn_classifier.py
+++ model_module/qrnn_classifier.py
@@ -7,29 +7,42 @@ import torchqrnn.forget_mult
 from torchqrnn import QRNN
 
 class QRNNClassifier(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, label_size, batch_size, num_layers = 1, dropout = 0, zoneout = 0, window = 1, save_prev_x = False):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, label_size, batch_size, num_layers = 1, dropout = 0, zoneout = 0, window = 1, save_prev_x = False, use_gpu=True):
+        """
+        Create QRNN classification module
+        """
         super().__init__()
+        
+        #initialize properties
         self.hidden_dim = hidden_dim
         self.batch_size = batch_size
         self.num_layers = num_layers
+        self.use_gpu = use_gpu
+        
+        #create nn module
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
         self.qrnn = QRNN(embedding_dim, hidden_dim, dropout=dropout, zoneout=zoneout, window = window, save_prev_x = save_prev_x, num_layers=num_layers)
         self.dropout = nn.Dropout(dropout)
         self.hidden_to_label = nn.Linear(hidden_dim, label_size)
-        self.hidden = self.init_hidden()
+        self.init_hidden()
+        if use_gpu:
+            self.cuda()
     
     def init_hidden(self):
         """
-        Initialize weight of hidden
+        Initialize weight of hidden state
         """
-        # the first is the hidden h
-        # the second is the cell  c
-        return autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_dim).cuda())
+        self.hidden = autograd.Variable(torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+        if self.use_gpu:
+            self.hidden.cuda()
     
     def reset(self):
         self.qrnn.reset()
     
     def forward(self, sentence):
+        self.batch_size = sentence.data.shape[1]
+        self.init_hidden()
+        
         embeds = self.word_embeddings(sentence)
         x = embeds.view(len(sentence), self.batch_size, -1)
         out, self.hidden = self.qrnn(x, self.hidden)
@@ -37,5 +50,3 @@ class QRNNClassifier(nn.Module):
         y = self.hidden_to_label(out[-1])
         log_probs = F.log_softmax(y)
         return log_probs
-
-
--- train.py
+++ train.py
@@ -11,11 +11,9 @@ import numpy as np
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 
 from model_module.qrnn_classifier import QRNNClassifier
-from model_module.lstm_classifier import LSTMClassifier
 
 import torchwordemb
 
@@ -24,12 +22,12 @@ from data_module.data_preprocessor import *
 import os
 import random
 
-import tensorboard_logger
-
 import re
 import datetime
 
 import git
+
+from model_module.model_runner import ModelRunner
 
 np.random.seed(1)
 torch.manual_seed(1)
@@ -43,6 +41,14 @@ repo = git.Repo(os.getcwd())
 headcommit = repo.head.commit
 current_branch = repo.active_branch.name
 RESULT_PATH = "runs/runs_" + current_branch + "_" + time.strftime("%a_%d_%b_%Y_%H_%M", time.gmtime(headcommit.committed_date))
+SAVED_MODEL_FOLDER_PATH = os.path.join(RESULT_PATH, 'best_models')
+SAVED_MODEL_NAME = 'faq_best_model.model'
+LOG_FOLDER_PATH = os.path.join(RESULT_PATH, 'summaries')
+TRAIN_LOG_FOLDER_PATH = 'train'
+DEV_LOG_FOLDER_PATH = 'dev'
+
+print(TRAIN_LOG_FOLDER_PATH)
+print(DEV_LOG_FOLDER_PATH)
 
 EMBEDDING_DIM = 300
 HIDDEN_DIM = 256
@@ -55,72 +61,8 @@ ZONEOUT = 0.5
 WINDOW = 2
 SAVE_PREV_X = False
 MAX_TEXT_LENGHT = None
-
-def get_accuracy(truth, pred):
-    assert len(truth)==len(pred)
-    right = 0
-    for i in range(len(truth)):
-        if truth[i]==pred[i]:
-            right += 1.0
-    return right/len(truth)
-
-def evaluate(model, eval_iter, loss_function, i, eval_logger, name ='dev'):
-    if isinstance(model, QRNNClassifier):
-        model.reset()
-    model.eval()
-    avg_loss = 0.0
-    truth_res = []
-    pred_res = []
-    print(eval_iter)
-    for batch in eval_iter:
-        sent, label = batch.text, batch.label
-        label.data.sub_(1)
-        truth_res += list(label.data)
-        model.batch_size = len(label.data)
-        model.hidden = model.init_hidden()  # detaching it from its history on the last instance.
-        pred = model(sent)
-        pred_label = pred.data.max(1)[1]
-        pred_res += [x for x in pred_label]
-        loss = loss_function(pred, label)
-        avg_loss += loss.data[0]
-
-    avg_loss /= len(eval_iter)
-    acc = get_accuracy(truth_res, pred_res)
-    print(name + ' avg_loss:%g train acc:%g' % (avg_loss, acc))
-    eval_logger.log_value("accuracy", acc, i)
-    eval_logger.log_value("loss", avg_loss, i)
-    return acc
-
-def train_epoch(model, train_iter, loss_function, optimizer, text_field, label_field, i, train_logger):
-    if isinstance(model, QRNNClassifier):
-        model.reset()
-    model.train()
-    avg_loss = 0.0
-    count = 0
-    truth_res = []
-    pred_res = []
-    for batch in train_iter:
-        sent, label = batch.text, batch.label
-        label.data.sub_(1)
-        truth_res += list(label.data)
-        model.batch_size = len(label.data)
-        model.hidden = model.init_hidden()# detaching it from its history on the last instance.
-        pred = model(sent)
-        pred_label = pred.data.max(1)[1]
-        pred_res += [x for x in pred_label]
-        model.zero_grad()
-        loss = loss_function(pred, label)
-        avg_loss += loss.data[0]
-        count += 1
-        if count % 100 == 0:
-            print('epoch: %d iterations: %d loss :%g' % (i, count*model.batch_size, loss.data[0]))
-        loss.backward()
-        optimizer.step()
-    avg_loss /= len(train_iter)
-    acc = get_accuracy(truth_res,pred_res)
-    print('epoch: %d done!\ntrain avg_loss:%g , acc:%g'%(i, avg_loss, acc))
-    train_logger.log_value("accuracy", acc, i)
-    train_logger.log_value("loss", avg_loss, i)
+USE_GPU = True
+LEARNING_RATE = 1e-3
 
 def tokenizer(text): # create a tokenizer function
     text = text.lower()
@@ -131,11 +73,7 @@ text_field = data.Field(lower=True, tokenize=tokenizer, fix_length=MAX_TEXT_LENG
 label_field = data.Field(sequential=False)
 train_iter, dev_iter = load_iter(text_field, label_field, batch_size=BATCH_SIZE, path = DATASET_PATH, dev_ratio=DEV_RATIO)
 
-best_dev_acc = 0.0
-
-model = QRNNClassifier(embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, vocab_size=len(text_field.vocab),label_size=len(label_field.vocab)-1, batch_size=BATCH_SIZE, num_layers=LAYERS_NUM, dropout=DROPOUT, zoneout=ZONEOUT, window = WINDOW, save_prev_x = SAVE_PREV_X)
-#model = LSTMClassifier(embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, vocab_size=len(text_field.vocab),label_size=len(label_field.vocab)-1, batch_size=BATCH_SIZE, num_layers=LAYERS_NUM, dropout=DROPOUT)
-model = model.cuda()
+model = QRNNClassifier(embedding_dim=EMBEDDING_DIM, hidden_dim=HIDDEN_DIM, vocab_size=len(text_field.vocab),label_size=len(label_field.vocab)-1, batch_size=BATCH_SIZE, num_layers=LAYERS_NUM, dropout=DROPOUT, zoneout=ZONEOUT, window = WINDOW, save_prev_x = SAVE_PREV_X, use_gpu=USE_GPU)
 ''' 
 vocab, vec = torchwordemb.load_word2vec_bin("../dataset/GoogleNews-vectors-negative300.bin")
 text_field.vocab.set_vectors(vocab, vec, EMBEDDING_DIM)
@@ -147,34 +85,12 @@ model.word_embeddings.weight.requires_grad = False
 
 loss_function = nn.NLLLoss()
 update_parameter = filter(lambda p: p.requires_grad, model.parameters())
-optimizer = optim.Adam(update_parameter, lr = 1e-3)
+optimizer = optim.Adam(update_parameter, lr = LEARNING_RATE)
 #optimizer = optim.Adagrad(update_parameter, lr=1e-3)
 #optimizer = optim.RMSprop(update_parameter, lr=1e-3, alpha=0.99, eps=1e-8, weight_decay=5e-4)
 
-os.system('rm -rf ' + RESULT_PATH)
-train_logger = tensorboard_logger.Logger(RESULT_PATH + "/summaries/train/")
-dev_logger = tensorboard_logger.Logger(RESULT_PATH + '/summaries/dev/')
-
-no_up = 0
+mode_runner = ModelRunner(model, EPOCH, loss_function, optimizer, False, SAVED_MODEL_FOLDER_PATH, SAVED_MODEL_NAME, LOG_FOLDER_PATH, TRAIN_LOG_FOLDER_PATH, DEV_LOG_FOLDER_PATH)
 start_time = time.time()
-for i in range(EPOCH):
-    print('epoch: %d start!' % i)
-    train_epoch(model, train_iter, loss_function, optimizer, text_field, label_field, i, train_logger)
-    print('now best dev acc:',best_dev_acc)
-    dev_acc = evaluate(model,dev_iter, loss_function, i, dev_logger, 'dev')
-    if dev_acc > best_dev_acc:
-        best_dev_acc = dev_acc
-        os.system('rm ' + RESULT_PATH + '/best_models/mr_best_model_minibatch_acc_*.model')
-        os.system('mkdir ' + RESULT_PATH + '/best_models/')
-        print('New Best Dev!!!')
-        torch.save(model.state_dict(), RESULT_PATH + '/best_models/mr_best_model_minibatch_acc_' + str(int(dev_acc*10000)) + '.model')
-        no_up = 0
-    '''
-    else:
-        no_up += 1
-        if no_up >= 10:
-            break
-    '''
-
+mode_runner.learn(train_iter, dev_iter)
 print("Overall time elapsed {} sec".format(time.time() - start_time))
 
