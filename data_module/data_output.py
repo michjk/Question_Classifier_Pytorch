1f3fd7a71c9caee33f41c9776f45f26d7a771a4a
Michael Jonathan <michj@gmail.com>
2018-03-08 18:01:38 +0800

    logger wrapper

--- data_module/data_output.py
+++ data_module/data_output.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from pandas_ml import ConfusionMatrix
+from tensorboard_logger import Logger
 
 def createConfusionMatrix(truth_res, pred_res, path):
     confusion_matrix = ConfusionMatrix(truth_res, pred_res)
@@ -9,3 +10,11 @@ def createConfusionMatrix(truth_res, pred_res, path):
     plt.savefig(path)
     print(confusion_matrix)
     confusion_matrix.print_stats()
+
+class PlotLogger:
+    def __init__(self, path):
+        self.plot_instance = Logger(path)
+    
+    def log_value(self, name, value, step):
+        self.plot_instance.log_value(name, value, step)
+
