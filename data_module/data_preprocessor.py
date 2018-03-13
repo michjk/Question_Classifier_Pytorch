ac77e83aa7f1a9db5911e57857c42cd71b6decb7
Michael Jonathan <michj@gmail.com>
2018-03-14 01:58:46 +0800

    data preprocessor support divided file and saving vocab

--- data_module/data_preprocessor.py
+++ data_module/data_preprocessor.py
@@ -13,6 +13,8 @@ from utils import dotdict
 
 import torchwordemb
 
+import dill as pickle
+
 label_place= set(['New_York_City', 'New_Haven,_Connecticut', 'Portugal', 'Southampton'])
 label_event = set(['American_Idol', '2008_Sichuan_earthquake', '2008_Summer_Olympics_torch_relay', 'The_Blitz'])
 label_person = set(['Beyonce', 'Frederic_Chopin', 'Queen_Victoria', 'Muammar_Gaddafi', 'Napoleon', 'Gamal_Abdel_Nasser', 'Dwight_D._Eisenhower', 'Kanye_West'])
@@ -181,76 +183,6 @@ class SQuAD(data.Dataset):
 
         return (train_data, dev_data, test_data)
 
-class FAQ(data.Dataset):
-    def __init__(self, text_field, label_field, path=None, examples=None, **kwargs):
-        """Create an FAQ dataset instance given a path and fields.
-        Arguments:
-            text_field: The field that will be used for text data.
-            label_field: The field that will be used for label data.
-            path: Path to the data file.
-            examples: The examples contain all the data.
-            Remaining keyword arguments: Passed to the constructor of
-                data.Dataset.
-        """
-        # text_field.preprocessing = data.Pipeline(clean_str)
-        fields = [('text', text_field), ('label', label_field)]
-        if examples is None:
-            path = self.dirname if path is None else path
-            examples = []
-            
-            with codecs.open(path,'r','utf8') as f:
-                for line in f:
-                    tmp = line.split()
-                    x = " ".join(tmp[1:])
-                    #x = clean_str(x)
-                    y = tmp[0]
-                    examples.append(data.Example.fromlist([x, y], fields))
-        
-        super().__init__(examples, fields, **kwargs)
-    
-    @staticmethod
-    def sort_key(ex):
-        return len(ex.text)
-
-    @classmethod
-    def splits(cls, text_field, label_field, path, test_ratio, **kwargs):
-        examples = cls(text_field, label_field, path, **kwargs).examples
-        np.random.shuffle(examples)
-
-        test_length = -1*int(test_ratio*float(len(examples)))
-        train_examples, test_examples = examples[:test_length], examples[test_length:]
-
-        print('train:',len(train_examples),'test:',len(test_examples))
-
-        return (cls(text_field, label_field, examples=train_examples),
-                cls(text_field, label_field, examples=test_examples)
-                )
-    
-    @classmethod
-    def splits_cv(cls, text_field, label_field, path, test_ratio, n_splits,  **kwargs):
-        examples = cls(text_field, label_field, path, **kwargs).examples
-        np.random.shuffle(examples)
-
-        test_length = -1*int(test_ratio*float(len(examples)))
-        train_examples, test_examples = examples[:test_length], examples[test_length:]
-        train_examples = np.array(train_examples)
-
-        test_data = cls(text_field, label_field, examples=test_examples)
-        
-        train_data = []
-        dev_data = []
-        kfold_iter = KFold(n_splits=n_splits).split(train_examples)
-        print(train_examples)
-        for train_index, dev_index in kfold_iter:
-            train_part = train_examples[train_index].tolist()
-            dev_part = train_examples[dev_index].tolist()
-            train_data.append(cls(text_field, label_field, examples=train_part))
-            dev_data.append(cls(text_field, label_field, examples=dev_part))
-        
-        print('train:',len(train_data[0].examples),'test:',len(test_examples), 'dev:', len(dev_data[0].examples))
-
-        return (train_data, dev_data, test_data)
-
 
 def load_iter(text_field, label_field, batch_size, path, dev_ratio, cpu = None):
     print('loading data')
@@ -314,12 +246,38 @@ def tokenizer(text): # create a tokenizer function
     tokenizer_re = re.compile(r"[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", re.UNICODE) 
     return tokenizer_re.findall(text)
 
-def load_dataset(path, batch_size, max_text_length, embedding_dim, tokenizer = tokenizer, dev_ratio = 0.1, pretrained_word_embedding_name = "glove.6B.300d", pretrained_word_embedding_path = None, use_gpu = True):
+class CustomDataset(data.Dataset):
+    def __init__(self, examples, fields, **kwargs):
+        super().__init__(examples, fields, **kwargs)
+    
+    @staticmethod
+    def sort_key(ex):
+        return len(ex.text)
+
+def sort_key(ex):
+    return len(ex.text)
+
+def load_dataset(train_path, dev_path, batch_size, max_text_length, embedding_dim, tokenizer = tokenizer,
+    sort_key = sort_key, dev_ratio = 0.1, pretrained_word_embedding_name = "glove.6B.300d", pretrained_word_embedding_path = None,
+    saved_text_vocab_path = "text_vocab.pkl", saved_label_vocab_path = "label_vocab.pkl",
+    use_gpu = True):
     text_field = data.Field(lower=True, tokenize=tokenizer, fix_length=max_text_length)
     label_field = data.Field(sequential=False)
 
     print('loading data')
-    train_data, dev_data = FAQ.splits(text_field, label_field, path, dev_ratio)
+    train_data = data.TabularDataset(path=train_path, format='csv', skip_header=True, fields=[("text", text_field), ('label', label_field)])
+    dev_data = data.TabularDataset(path=dev_path, format='csv', skip_header=True, fields=[("text", text_field), ('label', label_field)])
+    
+    '''
+    examples = all_data.examples
+    np.random.shuffle(examples)
+    test_length = -1*int(dev_ratio*float(len(examples)))
+    train_data, dev_data = examples[:test_length], examples[test_length:]
+    
+    train_data = CustomDataset(train_data, [("text", text_field), ("label", label_field)])
+    dev_data = CustomDataset(dev_data, [("text", text_field), ("label", label_field)])
+    '''
+    #train_data, dev_data = FAQ.splits(text_field, label_field, path, dev_ratio)
 
     print('building vocab')
     text_field.build_vocab(train_data, dev_data)
@@ -332,9 +290,9 @@ def load_dataset(path, batch_size, max_text_length, embedding_dim, tokenizer = t
     
     train_iter, dev_iter = data.Iterator.splits(
         (train_data, dev_data), batch_sizes=(batch_size, len(dev_data)),
-        repeat=False, device = cpu, shuffle = True
+        repeat=False, device = cpu, shuffle = True, sort_key = sort_key
     )
-    
+
     vectors = None
 
     if pretrained_word_embedding_name == "word2vec":
@@ -345,6 +303,9 @@ def load_dataset(path, batch_size, max_text_length, embedding_dim, tokenizer = t
         text_field.vocab.load_vectors(pretrained_word_embedding_name)
         vectors = text_field.vocab.vectors
     
+    pickle.dump(text_field, open(saved_text_vocab_path, 'wb'))
+    pickle.dump(label_field, open(saved_label_vocab_path, 'wb'))
+
     vocab_size = len(text_field.vocab)
     print("vocab size ", vocab_size)
     #from zero
