fb26ba0f5bf229b58be1614b75df6c148e42417c
Michael Jonathan <michj@gmail.com>
2018-03-14 19:39:38 +0800

    rest api server update

--- rest_api_server.py
+++ rest_api_server.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask import request
 
-from model_module.cnn_classifier import CNNClassifier
-from model_module.qrnn_classifier import QRNNClassifier
-from model_module.lstm_classifier import LSTMClassifier
-
 import torch
 from torchtext import data
 
-from data_module.data_preprocessor import QuestionWrapper, load_iter
+from data_module.data_preprocessor import QuestionWrapper
 
 import os
 import re
@@ -17,46 +13,37 @@ from flask import jsonify
 
 import logging
 
+import dill as pickle
+
+import argparse
+
+from utils import *
+
 app = Flask(__name__)
 
-MODEL_STATE_PATH = "faq_best_model.model"
+MODEL_STATE_PATH = "runs/refactor_Tue_13_Mar_2018_17_58/best_model/faq_best_model.model"
 
-DATASET_FOLDER = os.path.join("..", "dataset")
-DATASET_PATH = os.path.join(DATASET_FOLDER, "faqs", "faq_ntu_prototype_v4.txt")
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", help="path parameter json file")
+param_json_path = parser.parse_args().path
 
-EMBEDDING_DIM = 300
-EPOCH = 400
-BATCH_SIZE = 64
-DEV_RATIO = 0.1
-DROPOUT = 0.5
-KERNEL_SIZES = [3, 4, 5]
-KERNEL_NUM = 128
-MAX_TEXT_LENGHT = 81
+param = load_parameters_from_json(param_json_path)
 
-def tokenizer(text): # create a tokenizer function
-    text = text.lower()
-    TOKENIZER_RE = re.compile(r"[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", re.UNICODE) 
-    return TOKENIZER_RE.findall(text)
-
-text_field = data.Field(lower=True, tokenize=tokenizer, fix_length=MAX_TEXT_LENGHT)
-label_field = data.Field(sequential=False)
-train_iter, dev_iter = load_iter(text_field, label_field, batch_size=BATCH_SIZE, path = DATASET_PATH, dev_ratio=DEV_RATIO)
-
-text_field.vocab.load_vectors('glove.6B.300d')
-
-model = CNNClassifier(EMBEDDING_DIM, len(text_field.vocab), len(label_field.vocab)-1, BATCH_SIZE,KERNEL_NUM, KERNEL_SIZES, DROPOUT)
-#model = model.cuda()
-model.load_state_dict(torch.load(MODEL_STATE_PATH))
-
-if (os.path.isfile("rest_api_logging.log")):
+model = None
+if param.use_gpu:
+    model = torch.load(param.saved_model_file_path)
+else:
+    model = torch.load(param.saved_model_file_path, map_location=lambda storage, location: storage)
+""" 
+if (os.path.isfile(path.saved_model_file_path)):
     os.remove("rest_api_logging.log")
-
+"""
 # Create the Logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
  
 # Create the Handler for logging data to a file
-logger_handler = logging.FileHandler("rest_api_logging.log")
+logger_handler = logging.FileHandler(param.log_file_path)
 logger_handler.setLevel(logging.DEBUG)
  
 # Create a Formatter for formatting the log messages
@@ -69,18 +56,24 @@ logger_handler.setFormatter(logger_formatter)
 logger.addHandler(logger_handler)
 logger.info('Completed configuring logger()!')
 
-def preprocess_question(question):
+text_field = pickle.load(open(param.text_vocab_path, "rb"))
+label_field = pickle.load(open(param.label_vocab_path, "rb"))
+
+def preprocess_question(question, transpose = False, use_gpu = False):
+    device = -1
+    if use_gpu:
+        device = None
     question_data = QuestionWrapper(text_field, question)
     _, question_iter = data.Iterator.splits(
         (question_data, question_data), batch_size=len(question_data),
-        repeat=False, device = -1
+        repeat=False, device = device
     )
-    res = None
 
     for batch in question_iter:
         text = batch.text
-        text.data.t_()
-        
+        if transpose:
+            text.data.t_()
+
         return text
 
 def get_label(label_tensor):
@@ -100,9 +93,11 @@ def prediction():
         question = request.get_json()['question']
         print(question)
         logger.info(question)
-        x = preprocess_question(question)
+        x = preprocess_question(question, transpose = param.use_gpu, use_gpu=param.use_gpu)
+        print("preprocess complete")
         model.eval()
         y = model(x)
+        print("model inference")
         label_string = get_label(y)
         print(label_string)
         return jsonify({'result': str(label_string)})
@@ -112,4 +107,4 @@ def prediction():
         return response
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host=param.host)
--- train_lstm.py
+++ train_lstm.py
@@ -33,23 +33,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--path", help="path parameter json file")
 param_json_path = parser.parse_args().path
 
-parameter = load_parameters_from_json(param_json_path)
-qrnn_parameter = filter_dotdict_class_propoperty(parameter, LSTMClassifier)
+param = load_parameters_from_json(param_json_path)
+qrnn_parameter = filter_dotdict_class_propoperty(param, LSTMClassifier)
 
 train_iter, dev_iter, vocab_size, label_size, pretrained_embedding_weight = load_dataset(
-    parameter.dataset_path, parameter.batch_size, parameter.max_text_length, parameter.embedding_dim, dev_ratio = parameter.dev_ratio, pretrained_word_embedding_name = parameter.pretrained_word_embedding_name, pretrained_word_embedding_path = parameter.pretrained_word_embedding_path, use_gpu = parameter.use_gpu
+    param.train_dataset_path, param.dev_dataset_path, param.batch_size, param.max_text_length, param.embedding_dim, pretrained_word_embedding_name = param.pretrained_word_embedding_name, pretrained_word_embedding_path = param.pretrained_word_embedding_path, use_gpu = param.use_gpu
     )
 
 model = LSTMClassifier(**qrnn_parameter, vocab_size=vocab_size,label_size=label_size, pretrained_embedding_weight=pretrained_embedding_weight)
 
 loss_function = nn.NLLLoss()
 update_parameter = filter(lambda p: p.requires_grad, model.parameters())
-optimizer = optim.Adam(update_parameter, lr = parameter.learning_rate)
+optimizer = optim.Adam(update_parameter, lr = param.learning_rate)
 #optimizer = optim.Adagrad(update_parameter, lr=1e-3)
 #optimizer = optim.RMSprop(update_parameter, lr=parameter.learning_rate, alpha=0.99, eps=1e-8, weight_decay=5e-4)
 
-learning_logger = LearningLogger(parameter.result_folder_path, parameter.saved_model_file_path, parameter.train_log_folder_path, parameter.dev_log_folder_path)
-mode_runner = ModelRunner(model, parameter.epoch, loss_function, optimizer, learning_logger,  parameter.transpose)
+learning_logger = LearningLogger(param.result_folder_path, param.saved_model_file_path, param.train_log_folder_path, param.dev_log_folder_path)
+mode_runner = ModelRunner(model, param.epoch, loss_function, optimizer, learning_logger,  param.transpose)
 start_time = time.time()
 mode_runner.learn(train_iter, dev_iter)
 print("Overall time elapsed {} sec".format(time.time() - start_time))
