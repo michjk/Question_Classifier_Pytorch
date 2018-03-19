3f5ea6a0a49203900a2a6db741e0f3ed41cd83b5
Michael Jonathan <michjk@users.noreply.github.com>
2018-03-19 20:19:13 +0800

    cnn cv

---
+++ train_cnn_cv.py
@@ -0,0 +1,62 @@
+import time
+
+import numpy as np
+
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.optim as optim
+
+from model_module.cnn_classifier import CNNClassifier
+
+from data_module.data_preprocessor import *
+
+import os
+import random
+
+import datetime
+
+from model_module.model_runner import ModelRunner
+
+from utils import load_parameter_from_json, filter_dotdict_class_propoperty, FactoryClass
+
+from data_module.data_writer import LearningLogger
+
+import argparse
+
+np.random.seed(1)
+torch.manual_seed(1)
+torch.cuda.manual_seed_all(1)
+random.seed(1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", help="path parameter json file")
+param_json_path = parser.parse_args().path
+
+param = load_parameter_from_json(param_json_path)
+cnn_parameter = filter_dotdict_class_propoperty(param, CNNClassifier)
+
+train_data, dev_data, vocab_size, label_size, label_map, pretrained_embedding_weight = load_dataset(
+    param.train_dataset_path, param.dev_dataset_path, param.max_text_length, param.embedding_dim, 
+    pretrained_word_embedding_name = param.pretrained_word_embedding_name, pretrained_word_embedding_path = param.pretrained_word_embedding_path
+)
+
+cnn_parameter.vocab_size = vocab_size
+cnn_parameter.label_size = label_size
+cnn_parameter.pretrained_embedding_weight = pretrained_embedding_weight
+
+model_factory = FactoryClass(CNNClassifier, qrnn_parameter)
+
+loss_factory = FactoryClass(nn.NLLLoss)
+
+optimizer_param_dict = filter_dotdict_class_propoperty(param, optim.Adam)
+optimizer_factory = FactoryClass(optim.Adam, optimizer_param_dict)
+
+#optimizer = optim.Adagrad(update_parameter, lr=1e-3)
+#optimizer = optim.RMSprop(update_parameter, lr=parameter.learning_rate, alpha=0.99, eps=1e-8, weight_decay=5e-4)
+
+learning_logger = LearningLogger(label_map, param.result_folder_path, param.saved_model_file_path, param.train_log_folder_path, param.dev_log_folder_path, param.confusion_matrix_folder_path)
+model_runner = ModelRunner(model_factory, loss_factory, optimizer_factory, param.epoch, param.batch_size, learning_logger, param.transpose, param.use_gpu)
+start_time = time.time()
+model_runner.learn_cv(train_data, dev_data, param.n_folds)
+print("Overall time elapsed {} sec".format(time.time() - start_time))
